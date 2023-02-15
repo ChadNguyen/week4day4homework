4b804fb2c66ccfd4a45d95ec95281eba5dc041f2
Chad Nguyen <chadn458@gmail.com>
2023-02-15 00:28:44 -0500

    first

---
+++ linkedlist.py
@@ -0,0 +1,50 @@
+class LinkedNode:
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+class LinkedList:
+    def __init__(self, head=None):
+        self.head = head
+
+    def append_node(self, value):
+        node = LinkedNode(value)
+        if self.head is None:
+            self.head = node
+            return
+        
+        current_node = self.head
+        while current_node.next_node is not None:
+            current_node = current_node.next_node
+        current_node.next_node = node
+
+    def remove_node(self, target_node_data):
+        if self.head is None:
+            raise Exception("List is empty")
+
+        if self.head.value == target_node_data:
+            self.head = self.head.next_node
+            return
+
+        previous_node = self.head
+        for node in self:
+            if node.value == target_node_data:
+                previous_node.next_node = node.next_node
+                return
+            previous_node = node
+
+    def printLinkedList(self):
+        current_node = self.head
+        while current_node is not None:
+            print(current_node.value,"->", end=" ")
+            current_node = current_node.next_node
+        print('None')
+
+values = [1, 31, 7, 1]
+l_list = LinkedList()
+for value in values:
+    l_list.append_node(value)
+l_list.printLinkedList()
+
+
+
